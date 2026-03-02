45359a5f2f1b7512e2d3728e305751273e65d9e6
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-02 21:26:52 +0530

    Add student list manager script

---
+++ practice/practice/student_list.py
@@ -0,0 +1,15 @@
+# Student List Manager
+
+students = ["Rohitt", "Amit", "Sukanyaa"]
+
+print("Student List:")
+for student in students:
+    print(student)
+
+print("Total Students:", len(students))
+
+students.append("Tanishq")
+
+print("Updated List:")
+for student in students:
+    print(student)
