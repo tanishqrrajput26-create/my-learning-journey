ca38193303bceace8df571a57bf99a3e64896384
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-02 21:25:03 +0530

    Create number_checker.py

---
+++ practice/number_checker.py
@@ -0,0 +1,15 @@
+# Number Checker Program
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("It is an Even number")
+else:
+    print("It is an Odd number")
+
+if num > 0:
+    print("It is Positive")
+elif num < 0:
+    print("It is Negative")
+else:
+    print("It is Zero")
