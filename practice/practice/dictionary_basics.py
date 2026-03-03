abd72f4cc0e77db59ff225484dbefa11746e3415
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-03 21:57:30 +0530

    Create dictionary_basics.py

---
+++ practice/practice/dictionary_basics.py
@@ -0,0 +1,17 @@
+# Learning Dictionary in Python
+
+student = {
+    "name": "Tanishq",
+    "age": 19,
+    "course": " Electornics and Computer "
+}
+
+print("Student Details:")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
+
+# Adding new data
+student["marks"] = 85
+
+print("Updated Student Data:", student)
