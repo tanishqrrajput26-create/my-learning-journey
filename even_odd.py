556700983412efe85fe06d166bdf8c8b8cf62ab8
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-06 10:52:41 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -8,3 +8,17 @@ if num % 2 == 0:
     print("The number is Even")
 else:
     print("The number is Odd")
+
+
+
+# Function to check even or odd
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+num = int(input("Enter a number: "))
+result = check_even_odd(num)
+
+print("The number is:", result)
