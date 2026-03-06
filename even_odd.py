ef6b5e7f970e2cf9e56c773cdbd2a38781ba4876
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-06 10:51:00 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,7 +1,9 @@
-# Program to check if a number is even or odd
+ # Program to check whether a number is even or odd
 
+# Take number input from user
 num = int(input("Enter a number: "))
 
+# Check if number is divisible by 2
 if num % 2 == 0:
     print("The number is Even")
 else:
