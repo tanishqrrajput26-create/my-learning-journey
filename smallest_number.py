272254b2129d69e05a31d9ed8c4e1f06928d456e
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-09 23:16:47 +0530

    Added program to find smallest number

---
+++ smallest_number.py
@@ -0,0 +1,9 @@
+# Program to find smallest of three numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+smallest = min(a, b, c)
+
+print("Smallest number is:", smallest)
