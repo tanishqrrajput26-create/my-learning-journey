2f1bfe7f0223739eb58da1f8927698d0b93cdece
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-10 22:39:50 +0530

    Create guess_number.py
    Added simple number guessing program

---
+++ guess_number.py
@@ -0,0 +1,10 @@
+# Simple number guessing game
+
+secret_number = 7
+
+guess = int(input("Guess the number (1-10): "))
+
+if guess == secret_number:
+    print("Correct guess!")
+else:
+    print("Wrong guess. Try again.")
