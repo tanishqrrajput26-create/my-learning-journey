5f1cdb6d33f11b0a4a12b16265c543d3ddda60ad
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-11 14:43:44 +0530

    Added menu driven calculator Program

---
+++ calculator_menu.py
@@ -0,0 +1,26 @@
+# Menu driven calculator
+
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = int(input("Enter your choice: "))
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == 1:
+    print("Result:", num1 + num2)
+
+elif choice == 2:
+    print("Result:", num1 - num2)
+
+elif choice == 3:
+    print("Result:", num1 * num2)
+
+elif choice == 4:
+    print("Result:", num1 / num2)
+
+else:
+    print("Invalid choice")
