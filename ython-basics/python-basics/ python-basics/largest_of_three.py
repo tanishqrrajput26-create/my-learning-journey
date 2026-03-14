bd639a5f6925ca005e3c4ade8c7caa89e52c5e31
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-14 23:21:42 +0530

    Added simple password generator project

---
+++ ython-basics/python-basics/ python-basics/largest_of_three.py
@@ -0,0 +1,9 @@
+# Program to find largest of three numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+largest = max(a, b, c)
+
+print("Largest number is:", largest)
