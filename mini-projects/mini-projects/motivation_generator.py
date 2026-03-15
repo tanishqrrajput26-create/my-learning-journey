1631c6025a4a2133b80d1edc15ef862fd49df36e
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-15 13:16:16 +0530

    Added motivational quote generator

---
+++ mini-projects/mini-projects/motivation_generator.py
@@ -0,0 +1,12 @@
+# Motivational quote generator
+
+import random
+
+quotes = [
+    "Believe in yourself!",
+    "Small progress is still progress.",
+    "Never stop learning.",
+    "Consistency beats motivation."
+]
+
+print(random.choice(quotes))
