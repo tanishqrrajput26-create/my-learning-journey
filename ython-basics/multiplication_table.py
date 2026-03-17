fd1d06f2e79ed35a42f3c1744aaa779954636c17
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-17 22:36:11 +0530

    Update multiplication_table.py

--- ython-basics/multiplication_table.py
+++ ython-basics/multiplication_table.py
@@ -4,3 +4,12 @@ num = int(input("Enter a number: "))
 
 for i in range(1, 11):
     print(num, "x", i, "=", num * i)
+
+# Example Input:
+# 5
+
+# Example Output:
+# 5 x 1 = 5
+# 5 x 2 = 10
+# ...
+# 5 x 10 = 50
