fc11afdc3eae0f9ef8a2ae2cd0a426ebb31c62bf
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-18 20:00:39 +0530

    Added quiz game project

---
+++ mini-projects/mini-projects/python-basics/mini-projects/quiz_game.py
@@ -0,0 +1,16 @@
+# Simple quiz game
+
+score = 0
+
+answer = input("What is 2 + 2? ")
+if answer == "4":
+    score += 1
+
+answer = input("What is capital of India? ")
+if answer.lower() == "delhi":
+    score += 1
+
+print("Your score is:", score)
+
+
+
