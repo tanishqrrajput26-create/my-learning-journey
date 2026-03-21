888526a98f397fcc73e5838bd95046af49e02f3f
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-21 21:37:38 +0530

    Added basic calculator program

---
+++ mini-projects/basic_calculator.py
@@ -0,0 +1,40 @@
+# Basic Calculator Program
+
+print("Simple Calculator")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter your choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == "1":
+    print("Result:", num1 + num2)
+
+elif choice == "2":
+    print("Result:", num1 - num2)
+
+elif choice == "3":
+    print("Result:", num1 * num2)
+
+elif choice == "4":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Cannot divide by zero")
+
+else:
+    print("Invalid choice")
+
+
+
+# Example Input:
+# 1
+# 5
+# 3
+
+# Example Output:
+# Result: 8
