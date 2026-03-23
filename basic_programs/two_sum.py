cba6ef8d14de0a06555073ea86ff0bb669f4c412
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-23 20:12:06 +0530

    Create two sum
     Use a dictionary (store numbers)
    
    Check: target - current number
    If already seen → answer mil gaya

---
+++ basic_programs/two_sum.py
@@ -0,0 +1,20 @@
+# Two Sum
+#Given numbers, find 2 numbers whose sum = target
+
+nums = [2, 7, 11, 15]
+target = 9
+
+seen = {}
+
+for i in range(len(nums)):
+    diff = target - nums[i]
+
+    if diff in seen:
+        print("Indices:", seen[diff], i)
+        break
+
+    seen[nums[i]] = i
+
+#nums = [2, 7, 11, 15]
+#target = 9
+#2 + 7 = 9 ✅
