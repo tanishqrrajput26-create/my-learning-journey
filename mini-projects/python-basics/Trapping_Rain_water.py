5c5a69fb1d78218a2b153bcebff46b1cdf0384d8
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-24 21:20:42 +0530

    Create  Trapping_Rain_water
    After rain, water gets trapped between bars
     Find total water stored

---
+++ mini-projects/python-basics/Trapping_Rain_water.py
@@ -0,0 +1,70 @@
+# Trapping Rain Water
+
+def trap(height):
+    left = 0
+    right = len(height) - 1
+
+    left_max = 0
+    right_max = 0
+
+    water = 0
+
+    while left < right:
+        if height[left] < height[right]:
+            if height[left] >= left_max:
+                left_max = height[left]
+            else:
+                water += left_max - height[left]
+            left += 1
+        else:
+            if height[right] >= right_max:
+                right_max = height[right]
+            else:
+                water += right_max - height[right]
+            right -= 1
+
+    return water
+
+
+# Input
+heights = list(map(int, input("Enter heights: ").split()))
+
+print("Trapped water:", trap(heights))
+
+# Trapping Rain Water
+
+def trap(height):
+    left = 0
+    right = len(height) - 1
+
+    left_max = 0
+    right_max = 0
+
+    water = 0
+
+    while left < right:
+        if height[left] < height[right]:
+            if height[left] >= left_max:
+                left_max = height[left]
+            else:
+                water += left_max - height[left]
+            left += 1
+        else:
+            if height[right] >= right_max:
+                right_max = height[right]
+            else:
+                water += right_max - height[right]
+            right -= 1
+
+    return water
+
+
+# Input
+#heights = list(map(int, input("Enter heights: ").split()))
+
+#print("Trapped water:", trap(heights))
+# Input:
+# 0 1 0 2 1 0 1 3 2 1 2 1
+
+# Output:
+# Trapped water: 6
