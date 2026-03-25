24ac0e8c63afab342189bb2321d2231bac5f7ea5
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-25 22:54:15 +0530

    Create Count_number_of_Sundays

---
+++ practice/practice/Count_number_of_Sundays.py
@@ -0,0 +1,25 @@
+# Count number of Sundays
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+start_day = input("Enter starting day: ").capitalize()
+n = int(input("Enter number of days: "))
+
+# Find starting index
+index = days.index(start_day)
+
+count = 0
+
+# Loop for n days
+for i in range(n):
+    current_day = days[(index + i) % 7]
+
+    if current_day == "Sunday":
+        count += 1
+
+print("Number of Sundays:", count)
+
+
+
+
+#output : id days=75 day=tuesday   then 10 sundays will be the o/p 
