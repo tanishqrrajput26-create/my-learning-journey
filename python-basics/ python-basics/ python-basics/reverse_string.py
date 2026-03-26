9c52fdf48e06a30a92a06d5daa575a293d449b6f
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-26 22:35:06 +0530

    Create reverse_string.py
     [::-1] means:
    
    Start → End
    Step = -1 (reverse direction)

---
+++ python-basics/ python-basics/ python-basics/reverse_string.py
@@ -0,0 +1,13 @@
+# Reverse a string
+
+text = input("Enter a string: ")
+
+reversed_text = text[::-1]
+
+print("Reversed string:", reversed_text)
+
+# Input:
+# hello
+
+# Output:
+# Reversed string: olleh
