aa3eab0878efc1f00cede05fda5dc7f9324e61d1
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-26 22:50:49 +0530

    Create square_pattern.py

---
+++ square_pattern.py
@@ -0,0 +1,13 @@
+ # Square pattern
+
+n = int(input("Enter size: "))
+
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+#* * * *
+#* * * *
+#* * * *
+#* * * *
