791d5ad47e4444fca49a77e0d317047e0d792c5e
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-27 21:29:50 +0530

    Create Diamond_pattern

---
+++ Diamond_pattern.py
@@ -0,0 +1,25 @@
+# Diamond pattern
+
+n = int(input("Enter rows: "))
+
+# Upper part
+for i in range(1, n+1):
+    print(" " * (n - i), end="")
+    for j in range(i):
+        print("* ", end="")
+    print()
+
+# Lower part
+for i in range(n-1, 0, -1):
+    print(" " * (n - i), end="")
+    for j in range(i):
+        print("* ", end="")
+    print()
+
+ #  *
+  #* *
+ #* * *
+#* * * *
+ #* * *
+  #* *
+   #*
