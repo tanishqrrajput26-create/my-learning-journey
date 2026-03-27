21333f446d0c56f07cb0e07819d7215b06e3753b
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-03-27 21:24:57 +0530

    Create  Pyramid_pattern

---
+++ Pyramid_pattern.py
@@ -0,0 +1,17 @@
+ # Pyramid pattern
+
+n = int(input("Enter rows: "))
+
+for i in range(1, n+1):
+    print(" " * (n - i), end="")
+    
+    for j in range(i):
+        print("* ", end="")
+    
+    print()
+
+
+#   *
+ # * *
+ #* * *
+#* * * *
