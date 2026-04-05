dc9bc51dcdb5b29719bfedf73623015a0945d070
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-04-05 22:51:06 +0530

    Update intersectionof2_arrays.py

--- intersectionof2_arrays.py
+++ intersectionof2_arrays.py
@@ -6,3 +6,10 @@ arr2 = list(map(int, input("Enter second array: ").split()))
 intersection = list(set(arr1) & set(arr2))
 
 print("Intersection:", intersection)
+
+#Input:
+#1 2 3 4
+#3 4 5 6
+
+#Output:
+#Intersection: [3, 4]
