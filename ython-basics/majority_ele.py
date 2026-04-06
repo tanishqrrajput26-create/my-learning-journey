df3e577c4393d8ef5f7d28d6cbd927b947aa29e2
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-04-06 22:46:18 +0530

    Update majority_ele

--- ython-basics/majority_ele.py
+++ ython-basics/majority_ele.py
@@ -10,4 +10,10 @@ for num in arr:
         candidate = num
     count += (1 if num == candidate else -1)
 
+#Input:
+#2 2 1 1 2 2 2
+
+#Output:
+#Majority element: 2
+
 print("Majority element:", candidate)
