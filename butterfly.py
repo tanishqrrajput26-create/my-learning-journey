553c9523725d05b91af4d9eba1be451e9a0ad83e
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-04-11 15:06:47 +0530

    Add newline to lower part of butterfly pattern
    Fix print statement to include newline for butterfly shape.

--- butterfly.py
+++ butterfly.py
@@ -12,4 +12,13 @@ for i in range(1, n+1):
 for i in range(n, 0, -1):
     print("*" * i, end="")
     print(" " * (2*(n-i)), end="")
-    print("*" * i)
+    print("*" * i) 
+ #Output:
+    *
+   **
+  ***
+ ***
+****
+  ***
+   **
+    *
