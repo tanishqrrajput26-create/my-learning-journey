0f98da1b8b972c29fd3729878ac75edb9cc3876b
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-04-14 22:47:45 +0530

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -4,7 +4,9 @@ def find_largest(a, b, c):
     return max(a, b, c)
 
 num1 = int(input("Enter first number: "))
+
 num2 = int(input("Enter second number: "))
+
 num3 = int(input("Enter third number: "))
 
 largest = find_largest(num1, num2, num3)
