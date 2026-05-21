cbb2a5ff0adcddc412c8c45fac8819be6c437a94
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-05-21 23:26:00 +0530

    Create hourhand.py

---
+++ python-basics/hourhand.py
@@ -0,0 +1,10 @@
+# Hours to Minutes Conversion Program
+
+# Taking input from user
+hours = int(input("Enter hours: "))
+
+# Converting hours into minutes
+minutes = hours * 60
+
+# Displaying output
+print("Total minutes:", minutes)
