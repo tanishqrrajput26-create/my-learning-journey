6c8b014cd4422fec5dff1d19caa3bb3b3078fb00
T@nishq Rajput <tanishqrrajput26@gmail.com>
2026-05-31 23:36:08 +0530

    temperature_converter.py

--- Create file: temperature_converter.py
+++ Create file: temperature_converter.py
@@ -5,4 +5,4 @@ celsius = float(input("Enter temperature in Celsius: "))
 
 fahrenheit = (celsius * 9/5) + 32
 
-print("Temperature in Fahrenheit:", fahrenheit)
+print("Temperature in Fahrenheit:",fahrenheit)
